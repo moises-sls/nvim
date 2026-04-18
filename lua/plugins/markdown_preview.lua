bd8f876afb3b4da6856a11aea5a6f4d42f02549e
moises-sls <moisessales262@gmail.com>
2026-04-17 21:31:45 -0300

    ajustando r.nvim, necessario atualizar: r.nvim, alterar o branch do treesitter para o main

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,13 +1,12 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "rmd", "markdown" }
-    vim.g.mkdp_auto_start = 1
-    vim.g.mkdp_command_for_global = 1
-
-
-  end,
-  ft = { "rmd", "markdown" },
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    -- Alterado de yarn para npm
+    build = "cd app && npm install",
+    init = function()
+        vim.g.mkdp_filetypes = { "rmd", "markdown" }
+        vim.g.mkdp_auto_start = 1
+        vim.g.mkdp_command_for_global = 1
+    end,
+    ft = { "rmd", "markdown" },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,39 @@
 return {
+    -- "nvim-treesitter/nvim-treesitter",
+    -- build = ":TSUpdate",
+    -- branch = "master",
+    -- opts = {
+    --     highlight = { enable = true },
+    --     indent = { enable = true },
+    --     auto_install = { true },
+    --     ensure_installed = {
+    --         "r",
+    --         "python",
+    --         "markdown",
+    --         "markdown_inline",
+    --         "bash",
+    --         "yaml",
+    --         "lua",
+    --         "vim",
+    --         "query",
+    --         "vimdoc",
+    --         "latex",
+    --         "html",
+    --     },
+    -- },
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-        auto_install = { true },
-        ensure_installed = {
-            "r",
-            "python",
-            "markdown",
-            "markdown_inline",
-            "bash",
-            "yaml",
-            "lua",
-            "vim",
-            "query",
-            "vimdoc",
-            "latex",
-            "html",
-        },
-    },
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local langs = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex", "csv" }
+        require("nvim-treesitter").install(langs)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = langs,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
